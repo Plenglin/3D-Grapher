108eb0dcf378216d8908e68f274f52e43b815eb3
Astrid Augusta Yu <astridyu3.14@gmail.com>
2016-04-08 00:37:33 -0700

    Minor change to rendering

--- 3dgraph.py
+++ 3dgraph.py
@@ -31,9 +31,9 @@ def generate3dPoints(func, xMin, xMax, yMin, yMax, xStep, yStep):
         y = yMin
         while y <= yMax:
             try:
-                a.append(matrix.columnvec(x, y, func(x, y), 1))
+                a.append(matrix.columnvec(x, func(x, y), y, 1))
             except ZeroDivisionError:
-                a.append(matrix.columnvec(x, y, 0, 0))
+                a.append(matrix.columnvec(x, 0, y, 1))
             y += yStep
         values.append(a)
         x += xStep
@@ -50,8 +50,8 @@ def render(display, points):
 
     o = transformation * matrix.columnvec(0, 0, 0, 1)
     x = transformation * matrix.columnvec(20, 0, 0, 1)
-    y = transformation * matrix.columnvec(0, 20, 0, 1)
-    z = transformation * matrix.columnvec(0, 0, 20, 1)
+    z = transformation * matrix.columnvec(0, 20, 0, 1)
+    y = transformation * matrix.columnvec(0, 0, 20, 1)
     pygame.draw.line(display, (255,0,0), (o[0,0], o[0,1]), (x[0,0], x[0,1]))
     pygame.draw.line(display, (0,255,0), (o[0,0], o[0,1]), (y[0,0], y[0,1]))
     pygame.draw.line(display, (0,0,255), (o[0,0], o[0,1]), (z[0,0], z[0,1]))
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 3D Grapher
 ==========
-You enter in a function in the form of z(x, y) into 3dgraph.py. You need pygame to use it. It renders with matrices.
+You enter in a function in the form of z(x, y) into 3dgraph.py. WASDQE controls the rotation.
+
+Requirements:
+
+- Pygame
